90fb6114085740779e2732184f81a536a7e99ee3
Matt Read <mread@spotd.co.uk>
2011-03-08 17:09:54 +0000

    double directory rename - can git detect this?

--- java/src/test/java/com/github/mread/calculators/moveexample/a/b/DummyMovedFile.java
+++
@@ -1,5 +0,0 @@
-package com.github.mread.calculators.moveexample.a.b;
-
-public class DummyMovedFile {
-
-}
---
+++ java/src/test/java/com/github/mread/calculators/moveexample/c/d/DummyMovedFile.java
@@ -0,0 +1,5 @@
+package com.github.mread.calculators.moveexample.c.d;
+
+public class DummyMovedFile {
+
+}
