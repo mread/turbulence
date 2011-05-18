20c7cfa6815995c184864d4643af292fd1afdaae
Matt Read <mread@spotd.co.uk>
2011-05-18 21:15:30 +0100

    less revealing

--- java/src/test/java/com/github/mread/turbulence4j/CommandLineTest.java
+++ java/src/test/java/com/github/mread/turbulence4j/CommandLineTest.java
@@ -52,7 +52,7 @@ public class CommandLineTest {
 
     @Test
     public void runAgainstSpecificTarget() {
-        CommandLine.main(new String[] { "/work/workspaces/Frame-git4/Frame", "R0701-IL..R0705-IL" });
+        CommandLine.main(new String[] { ".", "" });
         //        CommandLine.main(new String[] { "." });
     }
 }
